60ca9f24bc7836ce02e3767ef261335ae601f02e
Pruxis <llavaert@gmail.com>
2016-10-28 09:14:37 +0200

    Initialising project

---
+++ src/dotnet-crud-todo/Models/ITodoRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_crud_todo.Models {
+    interface ITodoRepository {
+        void Add(TodoItem item);
+        IEnumerable<TodoItem> GetAll();
+        TodoItem Find(string key);
+        TodoItem Remove(string key);
+        void Update(TodoItem item);
+    }
+}
---
+++ src/dotnet-crud-todo/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_crud_todo.Models {
+    public class TodoItem {
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public bool isComplete { get; set; }
+
+    }
+}
